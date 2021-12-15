716856c71eaabf4d5c560a3b5e5576b20494ce8d
Davide Volpe <volpe.davide.vd@gmail.com>
2021-12-15 13:26:34 +0100

    Update run_headless_analyzer.sh

--- batch_scripts/run_headless_analyzer.sh
+++ batch_scripts/run_headless_analyzer.sh
@@ -2,6 +2,5 @@
 /home/liger/ghidra/projects/crackme crackme \
 -import /home/liger/CM0588/crackme-bin-linux/crackme0x00 \
 -scriptPath /home/liger/GhidraProgramMeasures/ghidra_scripts \
--scriptPath /home/liger/GhidraProgramMeasures/src/main/java \
--postScript ProgramMeasuresScript.java analysis=halstead analyze-function=main export=json export-path=/home/liger/Desktop/lol.json \
+-postScript ProgramMeasuresScript.java analysis=halstead analyze-function=main export=json export-path=/home/liger/Desktop/test.json \
 -overwrite
