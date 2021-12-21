0bd52cc3c4062eb802a4fdcc030751e573ef4441
Davide Volpe <volpe.davide.vd@gmail.com>
2021-12-21 12:41:02 +0100

    #1: added argument for program analysis fix parsing of zero args split the scripts for different headless analysis test cases

---
+++ batch_scripts/run_ha_program.sh
@@ -0,0 +1,6 @@
+/home/liger/LIGERLabs/REtools/ghidra_9.2.4_PUBLIC/support/analyzeHeadless \
+/home/liger/ghidra/projects/crackme crackme \
+-import /home/liger/CM0588/crackme-bin-linux/crackme0x00 \
+-scriptPath /home/liger/GhidraProgramMeasures/ghidra_scripts \
+-postScript ProgramMeasuresScript.java analysis=halstead analyze-program export=json export-path=/home/liger/Desktop/test.json \
+-overwrite
--- src/main/java/it/westfox5/ghidra/script/args/Parser.java
+++ src/main/java/it/westfox5/ghidra/script/args/Parser.java
@@ -1,6 +1,7 @@
 package it.westfox5.ghidra.script.args;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
@@ -12,7 +13,6 @@ import it.westfox5.ghidra.export.Exporter.ExportType;
 import it.westfox5.ghidra.measure.AnalysisType;
 import it.westfox5.ghidra.measure.MeasuredProgram;
 import it.westfox5.ghidra.script.args.Argument.Operator;
-import it.westfox5.ghidra.util.StringUtils;
 
 public class Parser {
 	
@@ -29,9 +29,9 @@ public class Parser {
 		Iterator<String> tokens = split.iterator();
 		while (tokens.hasNext()) {
 			String token = tokens.next();
-			String[] argValue = token.split(ARG_VALUE_SEPARATOR);
-			Operator<?> op = Operator.byOpCode(argValue[0]);
-			Argument<?> arg = createArgument(op, argValue[1]);
+			List<String> argValues = new ArrayList<String>(Arrays.asList(token.split(ARG_VALUE_SEPARATOR)));
+			Operator<?> op = Operator.byOpCode(argValues.remove(0));
+			Argument<?> arg = createArgument(op, argValues);
 			
 			arguments.add(arg);
 		}
@@ -41,52 +41,45 @@ public class Parser {
 			.collect(Collectors.toMap(e -> e.getOperation(), Function.identity()));
 	}
 
-	private static Argument<?> createArgument(Operator<?> op, String value) {
+	private static Argument<?> createArgument(Operator<?> op, List<String> argValues) {
+		int numValuesProvided = argValues.size();
 		if (Operator.ANALYSIS_TYPE == op) {
-			if (op.getNumArgs() != 1) {
-				throw new IllegalArgumentException("Expecting only 1 value for the `analysis` argument.");
-			}
-			if (StringUtils.isEmpty(value)) {
-				throw new IllegalArgumentException("Expecting a value for the `analysis` argument but none was provided.");
+			if (numValuesProvided != op.getNumArgs()) {
+				throw new IllegalArgumentException("Expecting only 1 value for the `analysis` argument but "+argValues.size()+" was provided.");
 			}
 			
-			AnalysisType<?> analysisType = MeasuredProgram.getAnalysisTypeByName(value);
+			AnalysisType<?> analysisType = MeasuredProgram.getAnalysisTypeByName(argValues.get(0));
 			
 			return new Argument<AnalysisType<?>>(Operator.ANALYSIS_TYPE, analysisType);
 			
 		} else if(Operator.ANALYSIS_FUNCTION_NAME == op) {
-			if (op.getNumArgs() != 1) {
-				throw new IllegalArgumentException("Expecting only 1 value for the `analysis function name` argument.");
+			if (numValuesProvided != op.getNumArgs()) {
+				throw new IllegalArgumentException("Expecting only 1 value for the `analysis function name` argument but "+argValues.size()+" was provided.");
 			}
 			
-			if (StringUtils.isEmpty(value)) {
-				throw new IllegalArgumentException("Expecting a value for the `analysis function name` argument but none was provided.");
+			return new Argument<String>(Operator.ANALYSIS_FUNCTION_NAME, argValues);
+		
+		} else if(Operator.ANALYSIS_PROGRAM == op) {
+			if (numValuesProvided != op.getNumArgs()) {
+				throw new IllegalArgumentException("Expecting only 1 value for the `analysis program` argument but "+argValues.size()+" was provided.");
 			}
 			
-			return new Argument<String>(Operator.ANALYSIS_FUNCTION_NAME, value);
+			return new Argument<String>(Operator.ANALYSIS_PROGRAM);
 		
 		} else if(Operator.EXPORT_TYPE == op) {
-			if (op.getNumArgs() != 1) {
-				throw new IllegalArgumentException("Expecting only 1 value for the `export` argument.");
+			if (numValuesProvided != op.getNumArgs()) {
+				throw new IllegalArgumentException("Expecting only 1 value for the `export` argument but "+argValues.size()+" was provided.");
 			}
 			
-			if (StringUtils.isEmpty(value)) {
-				throw new IllegalArgumentException("Expecting a value for the `export` argument but none was provided.");
-			}
-			
-			ExportType exportType = ExportType.getExportTypeByName(value);
+			ExportType exportType = ExportType.getExportTypeByName(argValues.get(0));
 			
 			return new Argument<ExportType>(Operator.EXPORT_TYPE, exportType);
 		} else if (Operator.EXPORT_PATH == op) {
-			if (op.getNumArgs() != 1) {
-				throw new IllegalArgumentException("Expecting only 1 value for the `export path` argument.");
+			if (numValuesProvided != op.getNumArgs()) {
+				throw new IllegalArgumentException("Expecting only 1 value for the `export path` argument but "+argValues.size()+" was provided.");
 			}
 			
-			if (StringUtils.isEmpty(value)) {
-				throw new IllegalArgumentException("Expecting a value for the `export path` argument but none was provided.");
-			}
-			
-			return new Argument<String>(Operator.EXPORT_PATH, value);
+			return new Argument<String>(Operator.EXPORT_PATH, argValues);
 		} else {
 			throw new RuntimeException("Argument `"+op+"` not implemented.");
 		}
